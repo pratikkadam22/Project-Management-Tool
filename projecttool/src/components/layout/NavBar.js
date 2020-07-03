74053af81faf186fef48b005d1ce7567af24ae15
pratikkadam22 <pratikkadam2212@gmail.com>
2020-07-03 10:39:40 -0700

    layout elements created

---
+++ projecttool/src/components/layout/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+
+function NavBar() {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <Link to='/' className="brand-logo">Project Management Tool</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
