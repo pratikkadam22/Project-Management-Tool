88fc37e9d313e0a52b2af5b19bf40c72579144b8
pratikkadam22 <pratikkadam2212@gmail.com>
2020-07-04 23:29:40 -0700

    create project cloud function done

--- projecttool/src/components/dashboard/Notifications.js
+++ projecttool/src/components/dashboard/Notifications.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Notifications() {
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-5">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className="notification">
+                        <li>Notification</li>
+                        <li>Notification</li>
+                        <li>Notification</li>
+                        <li>Notification</li>
+                    </ul>
+                </div>
+            </div>            
         </div>
     )
 }
