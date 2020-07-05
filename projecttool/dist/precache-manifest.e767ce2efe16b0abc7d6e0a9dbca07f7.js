85536d4b417d0bfa3ab335d4fa0e49cd031f662f
pratikkadam22 <pratikkadam2212@gmail.com>
2020-07-05 00:49:36 -0700

    project deployed

---
+++ projecttool/dist/precache-manifest.e767ce2efe16b0abc7d6e0a9dbca07f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fd0a04bde19c3dfe535433b2060121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "660ff8aa9d7815ab50a9",
+    "url": "/static/css/main.3f93bc97.chunk.css"
+  },
+  {
+    "revision": "120dfb380e254c3dc972",
+    "url": "/static/js/2.7173f52d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.7173f52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660ff8aa9d7815ab50a9",
+    "url": "/static/js/main.d1af4b43.chunk.js"
+  },
+  {
+    "revision": "87f6d742ecbdc1172cf3",
+    "url": "/static/js/runtime-main.df15750b.js"
+  }
+]);
